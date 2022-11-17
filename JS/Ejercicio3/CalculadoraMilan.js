959309285ac9bbb912a016de7e70e21abe871539
uo265578 <71468124+uo265578@users.noreply.github.com>
2022-11-17 09:57:56 +0100

    Add files via upload

--- JS/Ejercicio3/CalculadoraMilan.js
+++ JS/Ejercicio3/CalculadoraMilan.js
@@ -4,58 +4,15 @@ class Calculadora {
 		this.consola="";
 		this.memoria="0";
 	}
+
+	numeros(num) {
+		var a = new Number(num);
+		this.consola+=a;
+		this.pintar();
+	}
 	  
-    numero9(){
-        this.consola+=9;
-        this.pintar();
-    }
-	
-	 numero8(){
-        this.consola+=8;
-        this.pintar();
-    }
-	
-	numero7(){
-        this.consola+=7;
-        this.pintar();
-    }
-	
-	numero6(){
-        this.consola+=6;
-        this.pintar();
-    }
-	
-	numero5(){
-        this.consola+=5;
-        this.pintar();
-    }
-	
-	numero4(){
-        this.consola+=4;
-        this.pintar();
-    }
-	
-	numero3(){
-        this.consola+=3;
-        this.pintar();
-    }
-	
-	numero2(){
-        this.consola+=2;
-        this.pintar();
-    }
-	
-	numero1(){
-        this.consola+=1;
-        this.pintar();
-    }
-	
-	numero0(){
-        this.consola+=0;
-        this.pintar();
-    }
-	
-	mSuma(){
+    	
+	mMas(){
 		this.memoria+="+" + document.getElementById('consola').value;
 		this.resetear();
 	}
@@ -90,6 +47,24 @@ class Calculadora {
 		this.pintar();
 	}
 
+	raiz(){
+		var aux = eval(this.consola);
+		aux = Math.sqrt(10, aux);
+		this.consola = aux;
+        this.memoria = this.consola;
+        this.pintar();
+	}
+
+	cambioSigno() {
+		if ("+" == this.consola.substring(0, 1)) {
+			this.consola = "-" + this.consola.substring(1, this.consola.length);
+		} else if ("-" == this.consola.substring(0, 1)) {
+			this.consola = "+" + this.consola.substring(1, this.consola.length);
+		} else
+			this.consola = "+" + this.consola.substring(0, this.consola.length);
+		this.memoria = this.consola;
+			this.pintar();
+	}
 	
 	pintar(){
         document.getElementById("consola").value = this.consola;
