bb96afa4fcc2f2ed8c1114a38641f11933ab208a
uo265578 <71468124+uo265578@users.noreply.github.com>
2022-12-06 05:48:36 +0100

    Add files via upload

--- JS/Ejercicio-11/Tarea6/Geolocalizacion.js
+++ JS/Ejercicio-11/Tarea6/Geolocalizacion.js
@@ -4,24 +4,31 @@ class GeoLocalizacion {
         navigator.geolocation.getCurrentPosition(this.getPosicion.bind(this), this.procesarErrores.bind(this));
     }
     getPosicion(posicion) {
-        this.oviedo = [posicion.coords.longitude,posicion.coords.latitude];
+        this.coordenadas = [posicion.coords.longitude,posicion.coords.latitude];
         this.apiKey = "pk.eyJ1IjoidW8yNzEyODgiLCJhIjoiY2t3Ynk4bG5hMGZhdjJwbjJzNjZwMHI2OCJ9.4VqHsw5zVVkrOGjQLn3doA";
         this.container = "mapa";
         this.mapStyle = "mapbox://styles/mapbox/dark-v10";
         this.mensaje = "Peticion correcta";
-        this.zoom = 12;
+        this.zoom = 15;
     }
     mostrarMapa() {
         mapboxgl.accessToken = this.apiKey;
         let map = new mapboxgl.Map({
           container: this.container,
           style: this.mapStyle,
-          center: this.oviedo,
+          center: this.coordenadas,
           zoom: this.zoom
         });
         let marker1 = new mapboxgl.Marker()
-            .setLngLat(this.oviedo)
+            .setLngLat(this.coordenadas)
             .addTo(map);
+    }
+
+    mostrarCoordenadas() {
+        var latitud = document.querySelector('input[name="latitud"]').value;
+        var longitud = document.querySelector('input[name="longitud"]').value;
+        this.coordenadas = [latitud, longitud];
+        this.mostrarMapa();
     }
 
     procesarErrores(error) {
