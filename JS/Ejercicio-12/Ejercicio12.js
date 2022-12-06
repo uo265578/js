f4e6bdc074f0a3ae51bcfd735bbb58a3a054328c
uo265578 <71468124+uo265578@users.noreply.github.com>
2022-12-06 12:53:04 +0100

    Add files via upload

--- JS/Ejercicio-12/Ejercicio12.js
+++ JS/Ejercicio-12/Ejercicio12.js
@@ -1,42 +1,35 @@
-var lector = new Object()
-lector.leer = function leerArchivoTexto(files) {
+class Procesar {
 
-    var archivo = files[0];
-    var nombre = document.getElementById("nombreArchivo");
-    var tamaño = document.getElementById("tamañoArchivo");
-    var tipo = document.getElementById("tipoArchivo");
-    var ultima = document.getElementById("ultimaModificacion");
-    var contenido = document.getElementById("contenidoArchivo");
-    var areaVisualizacion = document.getElementById("areaTexto");
-    var errorArchivo = document.getElementById("errorLectura");
-    nombre.innerText = "Nombre del archivo: " + archivo.name;
-    tamaño.innerText = "Tamaño del archivo: " + archivo.size + " bytes";
-    tipo.innerText = "Tipo del archivo: " + archivo.type;
-    ultima.innerText = "Fecha de la última modificación: " + archivo.lastModifiedDate;
-    contenido.innerText = "Contenido del archivo de texto:"
-
-    var tipoTexto = /text.*/;
-    var json = /json.*/
-    if (archivo.type.match(tipoTexto) || archivo.type.match(json)) {
-        var lector = new FileReader();
-        lector.onload = function (evento) {
-
-            areaVisualizacion.innerText = lector.result;
+    tamañoArchivos() {
+        var byte = 0,
+            archivos = document.getElementById("subirArchivos").files,
+            numArchivos = archivos.length;
+        for (var i = 0; i < numArchivos; i++) {
+            byte += archivos[i].size;
         }
-        lector.readAsText(archivo);
-    }
-    else {
-        errorArchivo.innerText = "Error : ¡¡¡ Archivo no válido !!!";
-    }
-};
+        var nombres = "";
+        for (var i = 0; i < numArchivos; i++) {
+            nombres += "Archivo[" + i + "] = " + archivos[i].name + archivos[i].size + archivos[i].type;
+        }
 
-lector.comprobarNav = function comprobarNav() {
-    if (window.File && window.FileReader && window.FileList && window.Blob) {
-
-        document.write("<p>Este navegador soporta el API File </p>");
+        document.querySelector("body > section > p:nth-child(4)").innerHTML += numArchivos;
+        document.querySelector("body > section > p:nth-child(5)").innerHTML += byte 
+        document.querySelector("body > section > p:nth-child(6)").innerHTML += nombres;
     }
-    else document.write("<p>¡¡¡ Este navegador NO soporta el API File y este programa puede no funcionar correctamente !!!</p>");
+
+
+
+    procesarArchivos(files) {
+        var archivo = files[0];
+        document.querySelector("body > section > p:nth-child(9)").innerText += archivo.name;
+        document.querySelector("body > section > p:nth-child(10)").innerText += archivo.size;
+        document.querySelector("body > section > p:nth-child(11)").innerText += archivo.type;
+        var procesar = new FileReader();
+        procesar.onload = function () {
+            document.querySelector("body > section > pre").innerText += procesar.result;
+        }
+        procesar.readAsText(archivo);
+    };
 }
 
-lector.comprobarNav()
-
+var procesador = new Procesar();
